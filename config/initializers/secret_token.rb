c9ebafc046ef12a6e7fbdfc8da2b857cba6b4e5f
MakerSquare Student <students@themakersquare.com>
2013-07-17 18:55:42 -0500

    initial commit of rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ativa::Application.config.secret_token = 'dbb17ed786b8c9d3544a44ca1933a8f40deb0bb675f1246b509dc20f7f466b4cda1cb4b6b9aa44fea54f182e357e7255f76cb4c3d8c94c57018263c60405b23a'
