5adb17350c654f3874f3d7670cdd071afadcde68
Shehzan Devani <shehzandevani@gmail.com>
2013-07-24 16:00:54 -0500

    Adding files for creating a user model and login sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+	def new
+	end
+
+
+	def create
+		user = User.find_by_email(params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			redirect_to user
+		else
+			flash[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
+	end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :name, :password, :password_confirmation
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  has_secure_password
+end
